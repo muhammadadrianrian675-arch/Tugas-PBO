f21cd0e972802ee9e19d657aa6fe0283a7064ad9
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-05 00:21:37 +0800

    Add BacaData class to read and display an integer

--- Tugas 1/Tugas1_SourceCode_Praktek/BacaData.java
+++ Tugas 1/Tugas1_SourceCode_Praktek/BacaData.java
@@ -1,1 +1,17 @@
+//Nama                  : Muh. Hamdan Syarif
+//NIM                   : 13020240240
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 20.00
 
+import java.util.Scanner;
+
+public class BacaData {
+    public static void main(String[] args) {
+        int a;
+        Scanner masukan;
+        System.out.print("Contoh membaca dan menulis, ketik nilai integer: \n");
+        masukan = new Scanner(System.in);
+        a = masukan.nextInt();
+        System.out.print("Nilai yang dibaca : " + a);
+        masukan.close();
+    }
+}
