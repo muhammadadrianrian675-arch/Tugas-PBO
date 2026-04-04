e50b60c6d12784f32d6f01e605094772bc37440a
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-05 00:34:52 +0800

    Update Oper3.java

--- Tugas 1/Tugas1_SourceCode_Praktek/Oper3.java
+++ Tugas 1/Tugas1_SourceCode_Praktek/Oper3.java
@@ -1,1 +1,22 @@
-
+//Nama                  : Muhammad Adrian
+//NIM                   : 13020240262
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 20.00
+    public class Oper3 {
+    public static void main(String[] args) {
+        if (true && true) {
+            System.out.println(true && true);
+        }
+        if (true & true) {
+            System.out.println(true & false);
+        }
+        if (true) {
+            System.out.println(true);
+        }
+        if (true || true) {
+            System.out.println(true);
+        }
+        if (true | false) {
+            System.out.println(true | false);
+        }
+    }
+}
