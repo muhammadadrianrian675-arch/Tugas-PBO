6f1eea36b3190051bfb8d7025e5727d3b79ac8c9
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-05 00:28:20 +0800

    Update Ekspresi1.java

--- Tugas 1/Tugas1_SourceCode_Praktek/Ekspresi1.java
+++ Tugas 1/Tugas1_SourceCode_Praktek/Ekspresi1.java
@@ -1,1 +1,27 @@
-
+//Nama                  : Muhammad Adrian
+//NIM                   : 13020240262
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 20.00
+    public class Ekspresi2 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        float fx;
+        float fy;
+        
+        System.out.print("x/y (format integer) = " + x / y);
+        System.out.print("\nx/y (format float) = " + x / y);
+        
+        fx = x;
+        fy = y;
+        System.out.print("\nx/y (format integer) = " + fx / fy);
+        System.out.print("\nx/y (format float) = " + fx / fy);
+        
+        System.out.print("\nfloat(x)/float(y) (format integer)= " + (float) x / (float) y);
+        System.out.print("\nfloat(x)/float(y) (format float) = " + (float) x / (float) y);
+        
+        x = 10;
+        y = 3;
+        System.out.print("\nx/y (format integer) = " + x / y);
+        System.out.print("\nx/y (format float) = " + x / y);
+    }
+}
