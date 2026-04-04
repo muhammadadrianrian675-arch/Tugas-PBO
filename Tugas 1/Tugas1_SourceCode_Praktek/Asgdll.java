8f10e4150ad767ce0183341cbac31bd8d489e458
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-05 00:14:53 +0800

    Update Asgdll.java

--- Tugas 1/Tugas1_SourceCode_Praktek/Asgdll.java
+++ Tugas 1/Tugas1_SourceCode_Praktek/Asgdll.java
@@ -1,4 +1,12 @@
-// NIM          : 13020240262
-// Nama         : Muhammad Adrian
-// Hari/Tanggal : 03/04/2026
+// NIM                : 13020240262
+// Nama               : Muhammad Adrian
+// Hari/Tanggal/Waktu : Jum'at/ 02 April 2026/ 20.00
 
+public class Asgdll {
+    public static void main(String[] args) {
+        float f = 20.0f;
+        double fll;
+        fll = 10.0f;
+        System.out.println("f : " + f + "\nfl1: " + fll);
+    }
+}
