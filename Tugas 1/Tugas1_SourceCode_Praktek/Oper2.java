df5378884fc5fa714e4bbe78c6d2943e63d83dec
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-05 00:33:59 +0800

    Add Oper2 class with bitwise and power operations

--- Tugas 1/Tugas1_SourceCode_Praktek/Oper2.java
+++ Tugas 1/Tugas1_SourceCode_Praktek/Oper2.java
@@ -1,1 +1,18 @@
-
+//Nama                  : Muhammad Adrian
+//NIM                   : 13020240262
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 20.00
+    public class Oper2 {
+    public static void main(String[] args) {
+        char i, j;
+        i = 3;
+        j = 4;
+        
+        System.out.println("i = " + (int) i);
+        System.out.println("j = " + (int) j);
+        System.out.println("i & j = " + (i & j));
+        System.out.println("i | j = " + (i | j));
+        System.out.println("i ^ j = " + (i ^ j));
+        System.out.println(Math.pow(i, j));
+        System.out.println("~i = " + ~i);
+    }
+}
