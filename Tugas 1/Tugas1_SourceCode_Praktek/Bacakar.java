00bf64a46938c2379a20c4aea22dcf34adb453e6
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-05 00:23:09 +0800

    Add Bacakar class for input handling examples

--- Tugas 1/Tugas1_SourceCode_Praktek/Bacakar.java
+++ Tugas 1/Tugas1_SourceCode_Praktek/Bacakar.java
@@ -1,1 +1,39 @@
+//Nama                  : Muhammad Adrian
+//NIM                   : 13020240262
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 20.00
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Bacakar {
+    public static void main(String[] args) throws IOException {
+        char cc;
+        int bil;
+        
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader dataIn = new BufferedReader(isr);
+        
+        System.out.print("hello\n");
+        System.out.print("baca 1 karakter : ");
+        cc = dataIn.readLine().charAt(0);
+        System.out.print("baca 1 bilangan : ");
+        bil = Integer.parseInt(dataIn.readLine());
+        
+        System.out.print(cc + "\n" + bil + "\n");
+        System.out.print("bye \n");
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("(Scanner) baca 1 bilangan : ");
+        int bilScanner = scanner.nextInt();
+        System.out.println("Nilai dari Scanner: " + bilScanner);
+        
+        String kar = JOptionPane.showInputDialog("Karakter 1 : ");
+        System.out.println("Karakter dari JOptionPane: " + kar);
+        JOptionPane.showMessageDialog(null, "hello");
+        
+        scanner.close();
+    }
+}
