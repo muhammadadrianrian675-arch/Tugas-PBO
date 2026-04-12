a728e8966393cae72c15ae2a8d220141ef03203a
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-13 00:16:50 +0800

    Create Ekspresi.java

---
+++ Tugas1/SourceCode/Ekspresi.java
@@ -0,0 +1,13 @@
+//Nama                  : Muhammad Adrian
+//NIM                   : 13020240262
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 20.00
+    public class Ekspresi {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        
+        System.out.print("x = " + x + "\n");
+        System.out.print("y = " + y + "\n");
+        System.out.print("hasil ekspresi = (x<y)?x:y = " + ((x < y) ? x : y));
+    }
+}
