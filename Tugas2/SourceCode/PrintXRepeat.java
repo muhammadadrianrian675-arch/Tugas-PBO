e6abc66f6ad55c50aaa51cee7beeaa8afb98c1b8
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-12 20:11:46 +0800

    Create PrintXRepeat.java

---
+++ Tugas2/SourceCode/PrintXRepeat.java
@@ -0,0 +1,42 @@
+/* NIM   : 13020240262
+Nama  : Muhammad Adrian
+Hari/Tanggal : 12 April 2026
+Waktu : 20:00*/
+
+import java.util.Scanner; 
+
+/* contoh baca nilai x, */ 
+/* Jumlahkan nilai yang dibaca dengan ITERATE */ 
+ 
+public class PrintXRepeat { 
+ 	
+	/** 
+	* @param args 
+	*/ 
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub 
+		/* Kamus : */ 
+		
+		int Sum; 
+		int x; 
+		Scanner masukan=new Scanner(System.in); 
+		/* Program */ 
+ 		
+		System.out.print ("Masukkan nilai x (int), akhiri dg 999 : "); 
+ 		
+		x = masukan.nextInt(); /* First Elmt */ 
+ 		
+		if (x == 999){ 
+			System.out.print("Kasus kosong \n"); 
+		}else { /* Minimal ada satu data yang dijumlahkan */ 	
+			Sum = 0; /* Inisialisasi; invariant !! */ 
+			do{ 
+				Sum = Sum + x; /* Proses */ 
+				System.out.print ("Masukkan nilai x (int), akhiri dg 999 : "); 
+				x = masukan.nextInt(); /* Next Elmt */ 
+			} while (x != 999); /* Kondisi pengulangan */ 
+			System.out.println  ("Hasil penjumlahan = "+Sum);
+			/* Terminasi */ 
+		} 
+	} 
+ }
