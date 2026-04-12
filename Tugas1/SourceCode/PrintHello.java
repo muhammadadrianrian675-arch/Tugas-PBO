4e89a3cbebf784935ca76f3b771757c746651665
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-13 00:21:14 +0800

    Create PrintHello.java

---
+++ Tugas1/SourceCode/PrintHello.java
@@ -0,0 +1,11 @@
+//Nama                  : Muhammad Adrian
+//NIM                   : 13020240262
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 20.00
+    public class PrintHello {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print("\nHello ");
+        System.out.println("World");
+        System.out.println("Welcome");
+    }
+}
