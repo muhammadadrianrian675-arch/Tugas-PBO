79f5e7be8bd0368488a16da205ba664590cc397a
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-12 19:14:32 +0800

    Add ForEver.java with a looping program

---
+++ Tugas2/SourceCode/ForEver.java
@@ -0,0 +1,20 @@
+/* NIM   : 13020240269
+Nama  : Muhammad Adrian
+Hari/Tanggal : 11 April 2026
+Waktu : 20:00 */
+
+public class ForEver {
+	
+	/**
+	* @param args
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Program */
+		System.out.println("Program akan looping, akhiri dengan ^c");
+		while (true)
+		{	System.out.print ("Print satu baris...\n");
+			break;
+		}
+	}
+}
