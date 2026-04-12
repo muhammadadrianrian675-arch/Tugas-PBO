797b51eeb48e8591bc6ef881a1a29f092fc5aecc
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-12 19:22:33 +0800

    Update KasusBoolean.java

--- Tugas2/SourceCode/KasusBoolean.java
+++ Tugas2/SourceCode/KasusBoolean.java
@@ -1,1 +1,28 @@
+/* NIM   : 13020240262
+Nama  : Muhammad Arian
+Hari/Tanggal : 11 April 2026
+Waktu : 20:00*/
 
+/* Eksrpesi kondisional dengan boolean */  
+public class KasusBoolean { 
+	
+	/** 
+	* @param args 
+	*/ 
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub 
+		/* Kamus */  
+		boolean bool; 
+		/* Algoritma */  
+		bool= true; 
+		if(bool) {  
+			System.out.print("true\n"); 
+		} else  
+			System.out.print("false\n");  
+		
+		if(!bool) {  
+			System.out.print("salah\n"); 
+		} else  
+			System.out.print("benar\n"); 
+	} 
+}
