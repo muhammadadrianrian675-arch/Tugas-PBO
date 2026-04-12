7286e1cecb2906178d7a47ec1bb29cb21f4cb117
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-12 16:29:21 +0800

    Add ASIGNI.java with basic data types and output

---
+++ Tugas 1/Tugas1_SourceCode_Praktek/SourceCode_Praktek/ASIGNI.java
@@ -0,0 +1,24 @@
+//Nama                  : Muhammad Adrian
+//NIM                   : 13020240262
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 20.00
+public class ASIGNi {
+    public static void main(String[] args) {
+        short ks = 1;
+        int ki = 1;
+        long kl = 10000;
+        char c = 65;
+        char cl = 'Z';
+        double x = 50.2f;
+        float y = 50.2f;
+        
+        System.out.println("Karakter = " + c);
+        System.out.println("Karakter = " + cl);
+        System.out.println("Karakter = " + c);
+        System.out.println("Karakter = " + cl);
+        System.out.println("Bilangan integer (short) = " + ks);
+        System.out.println("t(int) = " + ki);
+        System.out.println("t(long) = " + kl);
+        System.out.println("Bilangan Real x = " + x);
+        System.out.println("Bilangan Real y = " + y);
+    }
+}
