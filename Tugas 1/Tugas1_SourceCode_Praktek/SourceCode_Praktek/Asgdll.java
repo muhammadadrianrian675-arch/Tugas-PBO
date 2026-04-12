9dc5340557db55c8f31190f4c072be657ad31593
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-12 16:28:17 +0800

    Update and rename Asign.java to Asgdll.java

--- Tugas 1/Tugas1_SourceCode_Praktek/SourceCode_Praktek/Asgdll.java
+++ Tugas 1/Tugas1_SourceCode_Praktek/SourceCode_Praktek/Asgdll.java
@@ -1,10 +1,13 @@
 //Nama                  : Muhammad Adrian
 //NIM                   : 13020240262
 //Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 20.00
-public class Asign {
+
+
+public class Asgdll {
     public static void main(String[] args) {
-        int i;
-        System.out.print("hello\n");
-        i = 5;
-        System.out.println("Ini nilai i : " + i);}
+        float f = 20.0f;
+        double fll;
+        fll = 10.0f;
+        System.out.println("f : " + f + "\nfl1: " + fll);
+    }
 }
