2eb5dbb81bb3f1ee2c130bceb9d240274ac144fe
Muhammad Adrian <muhammadadrianrian675@gmail.com>
2026-04-12 16:35:52 +0800

    Create Lncr.java

---
+++ Tugas 1/Tugas1_SourceCode_Praktek/SourceCode_Praktek/Lncr.java
@@ -0,0 +1,11 @@
+//Nama                  : Muhammad Adrian
+//NIM                   : 13020240262
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 20.00
+    public class Incr {
+    public static void main(String[] args) {
+        int i, j;
+        i = 3;
+        j = i++;
+        System.out.println("Nilai i : " + (+i) + "\nNilai j : " + j);
+    }
+}
